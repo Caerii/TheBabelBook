d00016998062a97f57ebaf089d8f434c177871e9
macerisg <macerisg@clarkson.edu>
2023-03-30 20:57:03 -0400

    def create functions

--- treeNode.py
+++ treeNode.py
@@ -3,3 +3,9 @@ from baseObject import baseObject
 class tree(baseObject):
     def __init__(self):
         self.setup('TreeNode')
+    def toList(self):
+        l = []
+        for row in self.data:
+            s = f"{row['ParentNodeID']} ({row['NodeLabel']} {row['NodeData']} {row['NodeLevel']})"  
+            l.append(s)
+        return l
